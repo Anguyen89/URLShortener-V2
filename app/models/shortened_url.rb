0f519d96d6388b74c1c32fa21809593937822eb4
Albert Nguyen <Albert.la.nguyen@gmail.com>
2016-07-05 15:41:37 -0700

    added command line interface

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -42,12 +42,6 @@ class ShortenedUrl < ActiveRecord::Base
     random
   end
 
-  # def self.random_code
-  #   loop do
-  #     random_code = SecureRandom.urlsafe_base64(16)
-  #     return random_code unless ShortenedUrl.exists?(short_url: random_code)
-  #   end
-  # end
 
   def self.create_for_user_and_long_url!(user, long_url)
     ShortenedUrl.create!(
